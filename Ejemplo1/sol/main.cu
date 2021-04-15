07cc9712a52826995e58945714dcff0fc1f0f2d7
christenis98 <42232457+christenis98@users.noreply.github.com>
2021-04-15 14:51:19 +0200

    Add files via upload

---
+++ Ejemplo1/sol/main.cu
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/time.h>
+
+//CUDA
+#include <cuda.h>
+
+double wtime(void)
+{
+        static struct timeval   tv0;
+        double time_;
+
+        gettimeofday(&tv0,(struct timezone*)0);
+        time_=(double)((tv0.tv_usec + (tv0.tv_sec)*1000000));
+        return( time_/1000000);
+}
+
+
+void addMatrix(float *a, float *b, float *c, int N)
+{
+	int i, j, idx;
+	for (i=0; i<N; i++)
+		for(j=0; j<N;j++){
+			idx = i*N+j;
+			a[idx]=b[idx]+c[idx];
+		}
+} 
+
+
+__global__ void addMatrixGPU(float *a, float *b, float *c, int N )
+{
+	int i = threadIdx.x + blockDim.x * blockIdx.x;
+	int j = threadIdx.y + blockDim.y * blockIdx.y;
+	int idx;
+	if (i<N && j < N)  {
+			idx = i*N+j;
+			a[idx]=b[idx]+c[idx];
+		}
+		
+}
+
+int main(int argc, char *argv[])
+{
+	float *a, *b, *c, *a_host;
+	float *a_GPU, *b_GPU, *c_GPU;
+
+	int i, j, N;
+
+	double t0, t1;
+
+
+	if(argc>1) {
+		N = atoi(argv[1]); printf("N=%i\n", N);
+	} else {
+		printf("Error!!!! \n ./exec number\n");
+	return (0);
+	}
+
+	// Mallocs CPU
+	a  = (float *)malloc(sizeof(float)*N*N);
+	b  = (float *)malloc(sizeof(float)*N*N);
+	c  = (float *)malloc(sizeof(float)*N*N);
+	for (i=0; i<N*N; i++){ b[i] = i-1; c[i] = i;}
+
+	/*****************/
+	/* Add Matrix CPU*/
+	/*****************/
+	t0 = wtime();
+	addMatrix(a, b, c, N);
+	t1 = wtime(); printf("Time CPU=%f\n", t1-t0);
+
+	/* Mallocs GPU */
+	cudaMalloc((void **)&a_GPU, N*N*sizeof(float));
+	cudaMalloc((void **)&b_GPU, N*N*sizeof(float));
+	cudaMalloc((void **)&c_GPU, N*N*sizeof(float));
+	
+	/* CPU->GPU */
+	cudaMemcpy(b_GPU, b, N*N*sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(c_GPU, c, N*N*sizeof(float), cudaMemcpyHostToDevice);
+
+	/*****************/
+	/* Add Matrix GPU*/
+	/*****************/
+	dim3 dimBlock(16, 16, 1	);
+	dim3 DimGrid(ceil(N/16), ceil(N/16), 1);
+	t0 = wtime();
+	addMatrixGPU<<<DimGrid,dimBlock>>>(a_GPU, b_GPU, c_GPU, N);
+	cudaThreadSynchronize();
+	t1 = wtime(); printf("Time GPU=%f\n", t1-t0);
+
+	/* GPU->CPU */
+	a_host  = (float *)malloc(sizeof(float)*N*N);
+	cudaMemcpy(a_host, a_GPU, N*N*sizeof(float), cudaMemcpyDeviceToHost);
+
+	/************/
+	/* Results  */
+	/************/
+	for (i=0; i<N; i++)
+		for (j=0; j<N; j++)
+			if(fabs(a[i*N+j]-a_host[i*N+j])>1e-5){
+				printf("a!=a_host in (%i,%i): ", i,j);
+				printf("A[%i][%i] = %f A_GPU[%i][%i]=%f\n", i, j, a[i*N+j], i, j, a_host[i*N+j] );
+			}
+
+	/* Free CPU */
+	free(a);
+	free(b);
+	free(c);
+	free(a_host);
+
+	/* Free GPU */
+	cudaFree(a_GPU);
+	cudaFree(b_GPU);
+	cudaFree(c_GPU);
+
+
+	return(1);
+}
+
---
+++ Ejemplo4/SOLUCION/matrix_mul.cu
@@ -0,0 +1,177 @@
+#include <stdio.h>
+#include "matrix_mul.h"
+
+// Thread block size
+#define BLOCK_SIZE 16 
+
+// Forward declaration of the device multiplication function
+__global__ void Muld(float*, float*, int, int, float*);
+
+// Host multiplication function
+// Compute C = A * B
+// hA is the height of A
+// wA is the width of A
+// wB is the width of B
+
+//export void Mul(float*, float*, int, int, int, float*);
+void Mul(float* A, float* B, int hA, int wA, int wB,
+	float* C)
+{
+	int size;
+
+	// Load A and B to the device
+	float* Ad;
+	size = hA * wA * sizeof(float);
+	cudaMalloc((void**)&Ad, size);
+	cudaMemcpy(Ad, A, size, cudaMemcpyHostToDevice);
+	float* Bd;
+	size = wA * wB * sizeof(float);
+	cudaMalloc((void**)&Bd, size);
+	cudaMemcpy(Bd, B, size, cudaMemcpyHostToDevice);
+
+	// Allocate C on the device
+	float* Cd;
+	size = hA * wB * sizeof(float);
+	cudaMalloc((void**)&Cd, size);
+
+	// Compute the execution configuration assuming
+	// the matrix dimensions are multiples of BLOCK_SIZE
+	dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
+	dim3 dimGrid(wB / dimBlock.x, hA / dimBlock.y);
+
+	// Launch the device computation
+	Muld<<<dimGrid, dimBlock>>>(Ad, Bd, wA, wB, Cd);
+
+	// Read C from the device
+	cudaMemcpy(C, Cd, size, cudaMemcpyDeviceToHost);
+
+	// Free device memory
+	cudaFree(Ad);
+	cudaFree(Bd);
+	cudaFree(Cd);
+}
+
+// Device multiplication function called by Mul()
+// Compute C = A * B
+// wA is the width of A
+// wB is the width of B
+__global__ void Muld(float* A, float* B, int wA, int wB, float* C)
+{
+	// Block index
+	int bx = blockIdx.x;
+	int by = blockIdx.y;
+
+	// Thread index
+	int tx = threadIdx.x;
+	int ty = threadIdx.y;
+
+	// Index of the first sub-matrix of A processed by the block
+	int aBegin = BLOCK_SIZE * wA * by;
+
+	// Index of the last sub-matrix of A processed by the block
+	int aEnd = aBegin + BLOCK_SIZE - 1;
+
+	// Step size used to iterate through the sub-matrices of A
+	int aStep = BLOCK_SIZE;
+
+	// Index of the first sub-matrix of B processed by the block
+	int bBegin = BLOCK_SIZE * bx;
+
+	// Step size used to iterate through the sub-matrices of B
+	int bStep = BLOCK_SIZE * wB;
+
+	// The element of the block sub-matrix that is computed
+	// by the thread
+	float Csub = 0;
+
+	// Loop over all the sub-matrices of A and B required to
+	// compute the block sub-matrix
+	for (int a = aBegin, b = bBegin; a <= aEnd; a += aStep, b += bStep) {
+		// Shared memory for the sub-matrix of A
+		__shared__ float As[BLOCK_SIZE][BLOCK_SIZE];
+
+		// Shared memory for the sub-matrix of B
+		__shared__ float Bs[BLOCK_SIZE][BLOCK_SIZE];
+
+		// Load the matrices from global memory to shared memory;
+		// each thread loads one element of each matrix
+		As[ty][tx] = A[a + wA * ty + tx];
+		Bs[ty][tx] = B[b + wB * ty + tx];
+		// Synchronize to make sure the matrices are loaded
+		__syncthreads();
+
+		// Multiply the two matrices together;
+		// each thread computes one element
+		// of the block sub-matrix
+		for (int k = 0; k < BLOCK_SIZE; ++k)
+			Csub += As[ty][k] * Bs[k][tx];
+
+		// Synchronize to make sure that the preceding
+		// computation is done before loading two new
+		// sub-matrices of A and B in the next iteration
+		__syncthreads();
+	}
+	
+	// Write the block sub-matrix to global memory;
+	// each thread writes one element
+	int bOut = wA * BLOCK_SIZE * by + BLOCK_SIZE * bx;
+	C[bOut + wA * ty + tx] = Csub;
+}
+
+
+/*int main(int argc,char** argv) {
+	float* d_A, *d_B, *d_C;
+	float* h_A, *h_B, *h_C;
+	int n = atoi(argv[1]);
+	int k =  atoi(argv[2]);
+	int m = atoi(argv[3]);
+
+	dim3 block(BLOCK_SIZE, BLOCK_SIZE);
+	dim3 grid(2,2);
+	cudaMalloc((void**)&d_A, n*k);
+	cudaMalloc((void**)&d_B, k*m);
+	cudaMalloc((void**)&d_C, n*m);
+	h_A = (float*)malloc(n*k);
+	h_B = (float*)malloc(k*m);
+	h_C = (float*)malloc(n*m);
+// Inicialización A y b
+	cudaMemcpy (d_A,h_A,n*k,cudaMemcpyHostToDevice);
+	cudaMemcpy (d_B,h_A,k*m,cudaMemcpyHostToDevice);
+	Muld<<<grid,block>>>(d_A,d_B, n,k, d_C);
+	cudaMemcpy(h_C,d_C,n*m,cudaMemcpyDeviceToHost);
+	cudaFree(d_A); 
+	cudaFree(d_B); 
+	cudaFree(d_C);
+	free(h_A); free(h_B); 
+	free(h_C);
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
